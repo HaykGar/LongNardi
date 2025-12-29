24f7b1b191a017a8b4d56079ca90450b67a109f3
Hayk Gargaloyan <haykgargaloyan@gmail.com>
2025-12-28 16:09:33 -0800

    Conv net and pipeline, dropout

--- DecisionEngine/nardi_net.py
+++ DecisionEngine/nardi_net.py
@@ -2,96 +2,124 @@ import os
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 import nardi  
-
-from abc import ABC, abstractmethod
-class IFeaturePipeline(ABC):    
+    
+class LegacyPipeline():
+    def __init__(self, flatten=True):
+        self.flatten=flatten
+            
     def handle_list_case(self, features : list[nardi.Features]):
-        xs = [self.__call__(f) for f in features]
-        return torch.stack(xs, dim=0)
-    
-    @abstractmethod
-    def __call__(self, features : list[nardi.Features]):
-        pass
-    
-class LegacyPipeline(IFeaturePipeline):
-    def __init__(self):
-        super().__init__()
-    
-    def __call__(self, features : list[nardi.Features]):
-        if isinstance(features, (list, tuple)):
-            return self.handle_list_case(features)
+        xs = [self.process_single(f) for f in features]
+        return torch.cat(xs, dim=0) # assumes batch dimension
         
-        board = torch.cat([
+    def process_board(self, features : nardi.Features):
+        return torch.cat([
             torch.from_numpy(features.player.occ).float(),
             torch.from_numpy(features.opp.occ).float()
         ], dim=0)
         
-        other = torch.tensor([
+    def process_scalars(self, features : nardi.Features):
+        return torch.tensor([
             features.player.pieces_off, 
             features.opp.pieces_off,
             features.player.sq_occ, 
             features.opp.sq_occ,
             features.player.pieces_not_reached, 
             features.opp.pieces_not_reached
-        ], dtype=torch.float32)
+        ], dtype=torch.float32).unsqueeze(1)
         
-        return torch.cat([board, other.unsqueeze(1)], dim=1).div_(15.0)
+    def process_single(self, features : nardi.Features):
+        board = self.process_board(features)
+        scalars = self.process_scalars(features)
+        
+        ret = torch.cat([board, scalars], dim=1).unsqueeze(0).div_(15.0)
+        if self.flatten:
+            return ret.flatten(-2)
+        else:
+            return ret
+    
+    def __call__(self, features : list[nardi.Features]):
+        if isinstance(features, (list, tuple)):
+            return self.handle_list_case(features)
+        return self.process_single(features)
+
+
 
 class NardiNet(nn.Module):
-    def __init__(self, n_h1, n_h2, feature_pipeline=None):
+    def __init__(self, n_h1, n_h2, feature_pipeline=None, input_dim=None, p_dropout=0):
         super().__init__()
         
         self.feature_to_tensor = feature_pipeline if feature_pipeline is not None else LegacyPipeline()
         
+        if input_dim == None:
+            eng = nardi.Engine()
+            dummy = self.feature_to_tensor(eng.board_features())
+            self.input_dim = dummy.shape[-1]
+            
+        else:
+            self.input_dim = input_dim   
+        
         self.trunk = nn.Sequential(
-            nn.Linear(6*25, n_h1),
+            nn.Linear(self.input_dim, n_h1),
             nn.SiLU(),
+            nn.Dropout(p=p_dropout),
             nn.Linear(n_h1, n_h2),
             nn.SiLU(),
+            nn.Dropout(p=p_dropout),
             nn.Linear(n_h2, 4)  # 4 logits for outcome probabilities
         )
-        # Scoring vector: +1, +2, -1, -2
-        self.register_buffer("scores", torch.tensor([1.0, 2.0, -1.0, -2.0]))   
         
-    # does not achieve same performance as legacy model, something is wrong...        
+        self.p_drop = p_dropout 
         
-    def forward(self, feat: nardi.Features) -> torch.Tensor:
-        x = self.feature_to_tensor(feat).flatten(1)
+        self.register_buffer("scores", torch.tensor([1.0, 2.0, -1.0, -2.0]))
+        
+    def forward_tensor(self, x : torch.Tensor):
         logits = self.trunk(x)              
         probs = torch.softmax(logits, dim=-1)
         return (probs * self.scores).sum(dim=-1)  # weighted sum
-
-# def SpatialNardiNet(nn.Module):
-#     def __init__(self):
-#        super().__init__()
-#        pass
-#        # ... 
-#        # ...
-
-#     def feat_to_tensor(self, features : nardi.Features | list[nardi.Features]):
-#         if isinstance(features, (list, tuple)):
-#             return handle_list_case(features, self.feat_to_tensor)
-        
-#         board = torch.cat([
-#             torch.from_numpy(features.player.occ).float(),
-#             torch.from_numpy(features.opp.occ).float()
-#         ], dim=0).div_(15.0)
-        
-#         global_info = torch.tensor([
-#             features.player.pieces_off, 
-#             features.opp.pieces_off,
-#             features.player.pip_count, 
-#             features.opp.pip_count,
-#             features.player.pieces_not_reached, 
-#             features.opp.pieces_not_reached
-#         ], dtype=torch.float32)
-        
-#         return board, global_info
+                
+    def forward(self, feat: nardi.Features) -> torch.Tensor:
+        x = self.feature_to_tensor(feat)
+        return self.forward_tensor(x)
     
-#     def forward(self, features : nardi.Features | list[nardi.Features]):
-#         board, global_info = self.feat_to_tensor(features)
-        
-        
     
+
+class ConvPipeline(LegacyPipeline):
+    def __init__(self):
+        super().__init__(flatten=False)
+        
+    def process_scalars(self, features):
+        return torch.tensor([
+            features.player.pieces_off, 
+            features.opp.pieces_off,
+            features.player.pip_count, 
+            features.opp.pip_count,
+            features.player.pieces_not_reached, 
+            features.opp.pieces_not_reached
+        ], dtype=torch.float32).unsqueeze(1)
+
+class ConvNardiNet(NardiNet):
+    def __init__(self, num_channels=8, dropout=0):
+        pipeline = ConvPipeline()
+        eng = nardi.Engine()
+        dummy = pipeline(eng.board_features())
+        super().__init__(64, 16, pipeline, input_dim=20*num_channels+dummy.shape[-2], p_dropout=dropout)
+        
+        # (B, 6, 24) board input processed as 1D sequence of 6 channels
+        self.conv = nn.Conv1d(6, num_channels, kernel_size=5)
+        self.norm = nn.LayerNorm(num_channels*20)
+        self.relu = nn.ReLU()
+        
+    def forward(self, feat : nardi.Features):
+        feat = self.feature_to_tensor(feat) # shape (B, 6, 25)    
+        board = feat[:, :, :-1]
+        scalars = feat[:, :, -1].squeeze(1)
+        
+        x = self.conv(board).flatten(1)
+        x = self.norm(x)
+        x = self.relu(x)
+        
+        x = torch.cat([x, scalars], dim=1)
+        return self.forward_tensor(x)
