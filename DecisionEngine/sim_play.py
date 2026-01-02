1fe3b0e1d6adc1de36eb167c49934987d6a9c87a
Hayk Gargaloyan <haykgargaloyan@gmail.com>
2026-01-01 17:48:05 -0800

    python side lookahead move

--- DecisionEngine/sim_play.py
+++ DecisionEngine/sim_play.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 
 from functools import partial
 
-from nardi_net import NardiNet
 import nardi
 from tqdm import tqdm
 
@@ -18,7 +17,7 @@ class Simulator:
         self.turn_num = 0        # turn number in current game
         
         self.win_rates = []
-        
+                
     def add_dirichlet_noise(self, priors, eps):
         n = len(priors)
         if(n <= 1):
@@ -45,6 +44,11 @@ class Simulator:
     def advance_turn(self):
         self.sign *= -1
         self.turn_num += 1
+        
+    def apply_board(self, board):
+        self.eng.apply_board(board)
+        self.advance_turn()
+        return None
 
     def apply_greedy_move(self, model, eval_only=True):
         options = self.eng.roll_and_enumerate() # no negation since we are looking from current perspective
@@ -56,7 +60,7 @@ class Simulator:
                 evals = model(options)
 
             best = options[int(evals.argmax().item())]
-            self.eng.apply_board(best.raw_data)
+            return self.apply_board(best.raw_data)
 
         self.advance_turn()
         
@@ -66,7 +70,7 @@ class Simulator:
         else:
             options = self.eng.roll_and_enumerate()
             if len(options) == 1:
-                self.eng.apply_board(options[0].raw_data)
+                return self.apply_board(options[0].raw_data)
             elif len(options) != 0:
                 evals = model(options).detach().cpu()
                 evals = evals - evals.max()
@@ -75,24 +79,78 @@ class Simulator:
                 noisy_priors = self.add_dirichlet_noise(priors, eps)
                 chosen_idx = np.random.choice(len(noisy_priors), p=noisy_priors)
                 chosen = options[chosen_idx]
-                self.eng.apply_board(chosen.raw_data)
+                return self.apply_board(chosen.raw_data)
             
             self.advance_turn()        
 
-# FIXME (Lookahead move) COMPLETELY BROKEN DUE TO REPRESENTATION CHANGE
-   
+    def evaluate_subtree(self, root : nardi.Node, evaluator):
+        if root.result is not None:
+            return root.result
+        elif root.is_leaf():
+            return evaluator(root.features)
+        else:
+            avg_opp_eval = 0    # avg eval from opponents perspective
+            for dice_grp in range(21):
+                # best eval opponent gets from one of these child positions
+                if not root.children_by_dice[dice_grp].data:
+                    opps_best = evaluator(root.features) * -1 
+                    # if no legal moves with dice, just consider current position
+                else:
+                    opps_best = -float("inf")
+                    
+                for child in root.children_by_dice[dice_grp].data:
+                    if child.result is not None:
+                        opps_best = child.result
+                        break 
+                    else:                        
+                        eval = self.evaluate_subtree(child, evaluator)
+                        opps_best = max(opps_best, eval)
+                avg_opp_eval += opps_best * root.children_by_dice[dice_grp].prob
+                
+            return avg_opp_eval * -1
+        
+    def apply_lookahead_move(self, evaluator):
+        print("doing lookahead move")
+        turn_over = not self.eng.roll()
+        if turn_over:
+            self.advance_turn()
+            return
+        
+        root = self.eng.tree_search(2)
+        if root.result is not None:
+            print("warning, tried to lookahead in terminal position")
+            return None
+        
+        children = root.children_by_dice[self.eng.dice_as_idx()].data
+                        
+        best_eval = -float("inf")
+        best_child = None
+        
+        for child in children:
+            if child.result is not None:
+                return self.apply_board(child.features.raw_data)
+            else:
+                eval = self.evaluate_subtree(child, evaluator)
+                if eval > best_eval:
+                    best_child = child
+                    best_eval = eval
+                    
+        if best_child is not None:
+            return self.apply_board(best_child.features.raw_data)
+        
+
     def play_random_move(self):
         options = self.eng.roll_and_enumerate()
         if len(options) != 0:               # at least 1 possible move   
             rand_idx = np.random.choice(len(options))
-            self.eng.apply_board(options[rand_idx].raw_data)
+            return self.apply_board(options[rand_idx].raw_data)
         self.advance_turn()
     
     def heuristic_move(self):
         options = self.eng.roll_and_enumerate()
         if len(options) != 0:               # at least 1 possible move 
             coverages = np.array([f.player.sq_occ for f in options])
-            self.eng.apply_board(options[coverages.argmax()].raw_data)
+            return self.apply_board(options[coverages.argmax()].raw_data)
         self.advance_turn()
         
     # TODO add more sophisticated heuristics and tie breakers for the above
@@ -114,7 +172,7 @@ class Simulator:
             if strat == "greedy":
                 return partial(self.apply_greedy_move, model=model, eval_only=True)
             elif strat == "lookahead":
-                raise ValueError("lookahead move selection currently unsupported")
+                return partial(self.apply_lookahead_move, evaluator=model)
             
         print("invalid model and strat provided, no valid function call found")
         return None
@@ -126,7 +184,7 @@ class Simulator:
 
         self.reset(position_setup)
         self.eng.Render()
-
+        
         while self.eng.should_continue_game():
             is_p1_move = (self.sign == 1 and not swap_order) or (self.sign == -1 and swap_order)
             if is_p1_move:
@@ -175,15 +233,26 @@ class Simulator:
                 
         return p1_score, p2_score
    
-    def play_with_graphics(self, opp_strat="human", opp_model=None):
+    def play_with_graphics(self, opp_strat="human", opp_model=None, from_endgame=False):
         p1_move = self.human_turn
         p2_move = self.strat_to_func(opp_model, opp_strat)
         
+        setup = self.config.withRandomEndgame if from_endgame else None
+        
         self.eng.AttachNewSFMLRW()
-        p1_last = self.simulate_game(p1_move, p2_move)
+        p1_last = self.simulate_game(p1_move, p2_move, position_setup=setup)
         if self.eng.is_terminal():
             print(f"congratulations player {2-p1_last}!")
             print("You Win!")
         else:
             print("exited due to quit")
-        
+      
+if __name__ =="__main__":
+    import nardi_net
+
+    model = nardi_net.ResNardiNet()
+    model.load_state_dict(torch.load("res.pt", map_location=torch.device('cpu'), weights_only=True))
+    model.eval()
+    
+    sim = Simulator()
+    sim.play_with_graphics("lookahead", model)
