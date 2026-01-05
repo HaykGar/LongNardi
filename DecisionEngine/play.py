f3ad526845d4eed47471f141b041d90921506a91
Hayk Gargaloyan <haykgargaloyan@gmail.com>
2026-01-04 23:46:20 -0800

    1 script for playing

---
+++ DecisionEngine/play.py
@@ -0,0 +1,18 @@
+import nardi
+from sim_play import Simulator
+import models
+
+model = None
+
+print("Enter \'c\' to play aganst computer or press Enter to play against human.")
+computer = input().lower() == 'c'
+
+if computer:
+    print("Enter \'e\' to play against easy difficulty or press Enter to play against hard difficulty.")
+    easy = input().lower() == 'e'
+    model = models.mlp_model if easy else models.res_model
+    
+strategy = "lookahead" if model else "human"
+
+sim = Simulator(sleep_time=1)
+sim.play_with_graphics(model, strategy)
